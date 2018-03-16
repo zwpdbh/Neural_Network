f5ba915f02710c61f3d60ba342a679450deb576f
zwpdbh <hyperion_z@outlook.com>
2018-03-17 12:07:00 +1300

    >

---
+++ cmake-build-debug/CMakeFiles/neural_network.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neural_network.dir/main.cpp.o"
+  "CMakeFiles/neural_network.dir/dataset.cpp.o"
+  "../bin/neural_network.pdb"
+  "../bin/neural_network"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neural_network.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
